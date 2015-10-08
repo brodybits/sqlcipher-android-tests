73f95fbb031d7d1bddae3c1225b0b5bbe7c0a310
Christopher J. Brody <brodybits@litehelpers.net>
2015-10-08 10:10:38 +0200

    Reproduce crash due to Unicode emoji characters (sqlcipher/android-database-sqlcipher#199)

--- src/main/java/net/zetetic/tests/UnicodeTest.java
+++ src/main/java/net/zetetic/tests/UnicodeTest.java
@@ -2,20 +2,33 @@ package net.zetetic.tests;
 
 import android.database.Cursor;
 import net.sqlcipher.database.SQLiteDatabase;
+import net.sqlcipher.database.SQLiteStatement;
 
 public class UnicodeTest extends SQLCipherTest {
     @Override
     public boolean execute(SQLiteDatabase database) {
-
         String expected = "КАКОЙ-ТО КИРИЛЛИЧЕСКИЙ ТЕКСТ"; // SOME Cyrillic TEXT
         String actual = "";
+
         Cursor result = database.rawQuery("select UPPER('Какой-то кириллический текст') as u1", new String[]{});
-        if(result != null){
+        if (result != null) {
             result.moveToFirst();
             actual = result.getString(0);
             result.close();
         }
-        return actual.equals(expected);
+        if (!actual.equals(expected)) return false;
+
+        // FUTURE TBD only run this on newer versions of Android once the following bug fixed:
+        // https://code.google.com/p/android/issues/detail?id=81341
+        if (false) {
+            // This will crash on all Android releases 1.X-5.X due Android bug 81341:
+            SQLiteStatement st = database.compileStatement("SELECT '\uD83D\uDE03'"); // SMILING FACE (MOUTH OPEN)
+            String res = st.simpleQueryForString();
+            if (!res.equals("\uD83D\uDE03")) return false;
+        }
+
+        // all ok:
+        return true;
     }
 
     @Override
